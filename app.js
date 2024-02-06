f4fe0d427d4465af3b4443ed8461e19e5a97e937
Khalid Alnasser <loody170ss@gmail.com>
2024-02-06 15:00:29 +0300

    prepared project for deployment

--- app.js
+++ app.js
@@ -9,10 +9,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const compression = require('compression');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
+app.use(compression());
 
 app.use(apiAuthMiddleware.apiAuthMiddleware);
 
